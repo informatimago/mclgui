067785f3151db4f40adddbe5879ab4fb9e78b4fb
Pascal J. Bourguignon <pjb@informatimago.com>
2017-10-17 04:22:59 +0200

    Added new files.

---
+++ ccl-patches.lisp
@@ -0,0 +1,78 @@
+;;;; -*- mode:lisp;coding:utf-8 -*-
+;;;;**************************************************************************
+;;;;FILE:               ccl-patches.lisp
+;;;;LANGUAGE:           Common-Lisp
+;;;;SYSTEM:             Common-Lisp
+;;;;USER-INTERFACE:     NONE
+;;;;DESCRIPTION
+;;;;    
+;;;;    Patches startup-ccl, so that if init-file is a list, all the
+;;;;    files in the list are loaded instead of a single one.
+;;;;    
+;;;;AUTHORS
+;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
+;;;;MODIFICATIONS
+;;;;    2017-08-04 <PJB> Added this header.
+;;;;BUGS
+;;;;    * this is not enough for the wanted feature of loading
+;;;;      init files in some order.
+;;;;LEGAL
+;;;;    GPL3
+;;;;    
+;;;;    Copyright Pascal J. Bourguignon 2017 - 2017
+;;;;    
+;;;;    This program is free software: you can redistribute it and/or modify
+;;;;    it under the terms of the GNU General Public License as published by
+;;;;    the Free Software Foundation, either version 3 of the License, or
+;;;;    (at your option) any later version.
+;;;;    
+;;;;    This program is distributed in the hope that it will be useful,
+;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;;    GNU General Public License for more details.
+;;;;    
+;;;;    You should have received a copy of the GNU General Public License
+;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;**************************************************************************
+
+(in-package "CCL")
+
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defparameter *saved-warn-if-redefine-kernel* ccl::*warn-if-redefine-kernel*)
+  (setf ccl::*warn-if-redefine-kernel* nil))
+
+(defun startup-ccl (&optional init-file)
+  ;; Many of the things done here could enter a break loop on error.
+  ;; If that break loop is exited via :q, quietly exit to here.
+  (catch :toplevel
+    (with-simple-restart (abort "Abort startup.")
+      (let ((init-files (if (listp init-file) init-file (list init-file))))
+        (dolist (init-file init-files)
+          (with-simple-restart (continue "Skip loading init file.")
+            ;; Don't return on success, but keep loading all the init-files provided.
+            (load init-file :if-does-not-exist nil :verbose nil))))
+      (flet ((eval-string (s)
+               (with-simple-restart (continue "Skip evaluation of ~a" s)
+                 (eval (read-from-string s))))
+             (load-file (name)
+               (with-simple-restart (continue "Skip loading ~s" name)
+                 (load name))))
+        (dolist (p *lisp-startup-parameters*)
+          (let* ((param (cdr p)))
+            (case (car p)
+              (:gc-threshold
+               (multiple-value-bind (n last) (parse-integer param :junk-allowed t)
+                 (when n
+                   (if (< last (length param))
+                       (case (schar param last)
+                         ((#\k #\K) (setq n (ash n 10)))
+                         ((#\m #\M) (setq n (ash n 20)))))
+                   (set-lisp-heap-gc-threshold n)
+                   (use-lisp-heap-gc-threshold))))
+              (:eval (eval-string param))
+              (:load (load-file param)))))))))
+
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (setf ccl::*warn-if-redefine-kernel* *saved-warn-if-redefine-kernel*))
---
+++ examples/picture-view.lisp
@@ -0,0 +1,62 @@
+;;;; -*- mode:lisp;coding:utf-8 -*-
+;;;;**************************************************************************
+;;;;FILE:               picture-view.lisp
+;;;;LANGUAGE:           Common-Lisp
+;;;;SYSTEM:             Common-Lisp
+;;;;USER-INTERFACE:     NONE
+;;;;DESCRIPTION
+;;;;    
+;;;;    A picture-view draws a picture read from a file.
+;;;;    
+;;;;AUTHORS
+;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
+;;;;MODIFICATIONS
+;;;;    2017-08-03 <PJB> Created.
+;;;;BUGS
+;;;;LEGAL
+;;;;    GPL3
+;;;;    
+;;;;    Copyright Pascal J. Bourguignon 2017 - 2017
+;;;;    
+;;;;    This program is free software: you can redistribute it and/or modify
+;;;;    it under the terms of the GNU General Public License as published by
+;;;;    the Free Software Foundation, either version 3 of the License, or
+;;;;    (at your option) any later version.
+;;;;    
+;;;;    This program is distributed in the hope that it will be useful,
+;;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;;    GNU General Public License for more details.
+;;;;    
+;;;;    You should have received a copy of the GNU General Public License
+;;;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;;**************************************************************************
+
+(defpackage "MCLGUI.EXAMPLE.PICTURE-VIEW"
+  (:use "COMMON-LISP" "MCLGUI")
+  (:export "PICTURE-VIEW"))
+(in-package "MCLGUI.EXAMPLE.PICTURE-VIEW")
+(enable-sharp-at-reader-macro)
+
+(defclass picture-view (view)
+  ((image :reader picture-view-image)
+   (file  :reader picture-view-image-file)))
+
+(defmethod initialize-instance :after ((view picture-view) &key image-file &allow-other-keys)
+  (when image-file
+    (setf (picture-view-image-file view) image-file)))
+
+(defmethod (setf picture-view-image-file) (image-file (view picture-view))
+  (check-type image-file (or string pathname file-stream))
+  (let ((path (pathname image-file)))
+    (setf (slot-value view 'image) (load-image path)
+          (slot-value view 'file)  path)))
+
+(defmethod view-draw-contents ((view picture-view))
+  (call-next-method)
+  (with-focused-view view
+    (draw-image (picture-view-image view)
+                (view-bounds view))))
+
+;;;; THE END ;;;;
+
